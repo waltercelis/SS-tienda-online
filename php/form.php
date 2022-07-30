53b79362dc694e20ce1424d0edf53c483b9e5141
Walter Celis <waltescelir22@gmail.com>
2022-07-30 05:31:08 -0300

    haciendo funcionar el formulario de contacto

--- php/form.php
+++ php/form.php
@@ -7,9 +7,9 @@ $check = $_POST['check'];
 $para = 'sportsshopoficial@gmail.com';
 $asunto = 'SportsShop SS';
 
-$mensaje .= "Este mensaje fue enviado por " . $email . " \r\n";
-$mensaje .= "Mensaje: " . $_POST['textarea'] . " \r\n";
-$mensaje .= $check . " \r\n";
+$mensaje .= "Enviado por " . $email . ",\r\n";
+$mensaje .= "Mensaje: " . $_POST['textarea'] . ",\r\n";
+$mensaje .= "Recibir mas información: " . $check . ",\r\n";
 $mensaje .= "Enviado el " . date("d/m/Y", time());
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
